390bdef31039eff018d59a7166cf95476e05e4e4
CyberPhoenix90 <Joao260690@gmail.com>
2020-07-04 19:55:34 +0200

    add aurum mouse

--- engine/src/input/mouse/mouse.ts
+++ engine/src/input/mouse/mouse.ts
@@ -1,125 +1,70 @@
 // import { CancellationToken } from 'aurumjs';
 
+import { CancellationToken, DataSource } from 'aurumjs';
+
 export enum MouseButtons {
 	LEFT = 0,
 	RIGHT = 2,
 	MIDDLE = 3
 }
 
-// export type AurumMouseEvent = MouseEvent & { propagationStopped: boolean };
+export class AurumMouse {
+	private heldDownButtons: { [key: number]: boolean };
+	private lastPositionX: number;
+	private lastPositionY: number;
+	private cancellationToken: CancellationToken;
+	private mouseDown: DataSource<MouseEvent>;
+	private mouseUp: DataSource<MouseEvent>;
 
-// export class AurumMouse {
-// 	/**
-// 	 * Fired whenever the browser fires a mouse down event
-// 	 */
-// 	public readonly onMouseDown: EventEmitter<{ e: AurumMouseEvent; camera: Camera2D }>;
-// 	/**
-// 	 * Fired whenever the browser fires a click event
-// 	 */
-// 	public readonly onClick: EventEmitter<{ e: AurumMouseEvent; camera: Camera2D }>;
-// 	/**
-// 	 * Fired whenever the browser fires a mouse up event
-// 	 */
-// 	public readonly onMouseUp: EventEmitter<{ e: AurumMouseEvent; camera: Camera2D }>;
-// 	/**
-// 	 * Fired whenever the browser fires a mouse move event
-// 	 */
-// 	public readonly onMouseMove: EventEmitter<MouseEvent>;
-// 	/**
-// 	 * Fired whenever the browser fires a wheel event
-// 	 */
-// 	public readonly onScroll: EventEmitter<{ e: WheelEvent; camera: Camera2D }>;
-// 	public get disposed(): boolean {
-// 		return this.cancellationToken.isCanceled;
-// 	}
+	constructor() {
+		this.cancellationToken = new CancellationToken();
+		this.heldDownButtons = {};
+		this.mouseDown = new DataSource();
+		this.mouseUp = new DataSource();
 
-// 	private cancellationToken: CancellationToken;
-// 	private heldDownButtons: { [key: number]: boolean };
-// 	private lastMouseMove: MouseEvent | undefined;
+		this.cancellationToken.registerDomEvent(document, 'mousemove', (e: any) => {
+			this.lastPositionX = (e as MouseEvent).clientX;
+			this.lastPositionY = (e as MouseEvent).clientY;
+		});
+		this.cancellationToken.registerDomEvent(document, 'mousedown', (e: any) => {
+			this.heldDownButtons[(e as MouseEvent).button] = true;
+			this.mouseDown.update(e);
+		});
+		this.cancellationToken.registerDomEvent(document, 'mouseup', (e: any) => {
+			this.heldDownButtons[(e as MouseEvent).button] = false;
+			this.mouseUp.update(e);
+		});
+	}
 
-// 	constructor(camera: Camera2D | Camera2D[]) {
-// 		this.heldDownButtons = {};
-// 		this.cancellationToken = new CancellationToken();
+	public listenMouseDown(key: MouseButtons): DataSource<MouseEvent> {
+		const result = new DataSource<MouseEvent>();
 
-// 		_.asArray(camera).forEach((c) => {
-// 			this.addCamera(c);
-// 		});
+		this.mouseDown.filter((e) => e.button === key).pipe(result);
 
-// 		this.onClick = new EventEmitter({ cancellationToken: this.cancellationToken });
-// 		this.onMouseDown = new EventEmitter({ cancellationToken: this.cancellationToken });
-// 		this.onMouseUp = new EventEmitter({ cancellationToken: this.cancellationToken });
-// 		this.onMouseMove = new EventEmitter({ cancellationToken: this.cancellationToken });
-// 		this.onScroll = new EventEmitter({ cancellationToken: this.cancellationToken });
+		return result;
+	}
 
-// 		this.cancellationToken.addCancelable(
-// 			stage.onPreDraw.subscribe(() => {
-// 				if (this.lastMouseMove) {
-// 					this.onMouseMove.fire(this.lastMouseMove);
-// 					this.lastMouseMove = undefined;
-// 				}
-// 			}).cancel
-// 		);
-// 	}
+	public listenMouseUp(key: MouseButtons): DataSource<MouseEvent> {
+		const result = new DataSource<MouseEvent>();
 
-// 	public createMouseDownAction(key: MouseButtons, existingAction?: ActionEmitter): ActionEmitter {
-// 		const result = existingAction || new EventEmitter();
+		this.mouseUp.filter((e) => e.button === key).pipe(result);
 
-// 		this.onMouseDown.subscribe((e) => {
-// 			if (e.e.button === key) {
-// 				const position = e.camera.projectMouseCoordinates(e.e);
-// 				result.fire({
-// 					points: [
-// 						{
-// 							x: position.x,
-// 							radius: 1,
-// 							y: position.y
-// 						}
-// 					],
-// 					state: true,
-// 					value: 1,
-// 					inputSource: InputSource.MOUSE,
-// 					type: InputType.DISCRETE
-// 				});
-// 			}
-// 		});
+		return result;
+	}
 
-// 		return result;
-// 	}
+	public getMouseX(): number {
+		return this.lastPositionX;
+	}
 
-// 	public addCamera(camera: Camera2D) {
-// 		this.cancellationToken.registerDomEvent(camera.view, 'mousedown', (e: AurumMouseEvent) => {
-// 			this.prepareEvent(e);
-// 			this.heldDownButtons[e.button] = true;
-// 			this.onMouseDown.fire({ e, camera });
-// 		});
-// 		this.cancellationToken.registerDomEvent(document, 'mouseup', (e: AurumMouseEvent) => {
-// 			this.prepareEvent(e);
-// 			delete this.heldDownButtons[e.button];
-// 			this.onMouseUp.fire({ e, camera });
-// 		});
-// 		this.cancellationToken.registerDomEvent(camera.view, 'click', (e: AurumMouseEvent) => {
-// 			this.prepareEvent(e);
-// 			this.onClick.fire({ e, camera });
-// 		});
-// 		this.cancellationToken.registerDomEvent(camera.view, 'mousemove', (e: MouseEvent) => {
-// 			this.lastMouseMove = e;
-// 		});
-// 		this.cancellationToken.registerDomEvent(camera.view, 'wheel', (e: WheelEvent) => {
-// 			this.onScroll.fire({ e, camera });
-// 		});
-// 	}
+	public getMouseY(): number {
+		return this.lastPositionY;
+	}
 
-// 	private prepareEvent(e: AurumMouseEvent) {
-// 		e.propagationStopped = false;
-// 		e.stopPropagation = () => {
-// 			e.propagationStopped = true;
-// 		};
-// 		e.stopImmediatePropagation = () => {
-// 			e.propagationStopped = true;
-// 		};
-// 	}
+	public isButtonDown(key: MouseButtons): boolean {
+		return this.heldDownButtons[key];
+	}
 
-// 	public dispose(): void {
-// 		this.cancellationToken.cancel();
-// 	}
-// }
+	public dispose(): void {
+		this.cancellationToken.cancel();
+	}
+}
